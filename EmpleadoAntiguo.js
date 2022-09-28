e2102dddcf91d46ae845132c120eca40b66f9b6f
sebastianlernoud <114444933+sebastianlernoud@users.noreply.github.com>
2022-09-27 23:24:12 -0300

    Update EmpleadoAntiguo.js

--- EmpleadoAntiguo.js
+++ EmpleadoAntiguo.js
@@ -29,38 +29,4 @@ EmpleadoAntiguo.prototype = Object.create(EmpleadoVentas.prototype);
 EmpleadoAntiguo.prototype.constructor = EmpleadoAntiguo;
 
 
-// class EmpleadoAntiguo extends Empleado{
-//     constructor(nombre,apellido,dni,pagoPorhora){
-//         super(nombre,apellido,dni);
-//         if (pagoPorhora<= 0){
-//             throw new Error("El pago por hora debe ser mayor a cero.");
-//         }
-//         this.pagoPorhora = pagoPorhora;
-//         this.facturas = [];
-//     }
-
-//     get salario(){
-//         return "El salario de "+this.nombre+" "+this.apellido+" es: " + this.calcularSalario();
-//     }
-
-//     calcularSalario(){
-//         let horasTotales = 8*5*4;
-//         let porcentaje = 0.15;
-//         let gananciaVentas = this.calcularVentas*porcentaje;
-//         return this.pagoPorhora*horasTotales + gananciaVentas;
-//     }
-
-//     get calcularVentas(){
-//         let total = 0;
-//         this.facturas.forEach(element => {
-//             total += element.calcularPrecio;
-//         });
-//         return total;
-//     }
-
-//     vender(factura){
-//         this.facturas.push(factura);
-//     }
-// }
-
 module.exports = EmpleadoAntiguo;
