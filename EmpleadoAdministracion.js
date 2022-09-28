96110f31c88000e9401c08ef06e7eed5a3944bb8
sebastianlernoud <114444933+sebastianlernoud@users.noreply.github.com>
2022-09-27 23:23:53 -0300

    Update EmpleadoAdministracion.js

--- EmpleadoAdministracion.js
+++ EmpleadoAdministracion.js
@@ -18,25 +18,4 @@ EmpleadoAdministracion.prototype = Object.create(Empleado.prototype);
 EmpleadoAdministracion.prototype.constructor = EmpleadoAdministracion;
 
 
-
-
-// class EmpleadoAdministracion extends Empleado{
-//     constructor(nombre,apellido,dni,pagoPorhora){
-//         super(nombre,apellido,dni);
-//         if (pagoPorhora<= 0){
-//             throw new Error("El pago por hora debe ser mayor a cero.");
-//         }
-//         this.pagoPorhora = pagoPorhora;
-//     }
-
-//     get salario(){
-//         return "El salario de "+this.nombre+" "+this.apellido+" es: " + this.calcularSalario();
-//     }
-
-//     calcularSalario(){
-//         let horasTotales = 8*5*4;
-//         return horasTotales*this.pagoPorhora;
-//     }
-// }
-
 module.exports = EmpleadoAdministracion;
